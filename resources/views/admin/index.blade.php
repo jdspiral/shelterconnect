babef07276bdb36ce519111492959c5ebbcc7737
Josh Hathcock <jdspiral@gmail.com>
2016-06-19 14:37:53 -0400

    Update index page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 6/19/16
- * Time: 12:29 PM
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+
+            </div>
+        </div>
+    </div>
+@endsection
