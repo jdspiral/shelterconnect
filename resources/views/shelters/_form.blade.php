fbdb4929c0cd0da3071c01ed20a203f87869eb5b
Josh Hathcock <jdspiral@gmail.com>
2016-06-19 14:39:10 -0400

    Create shelter form partial

--- resources/views/shelters/_form.blade.php
+++ resources/views/shelters/_form.blade.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 6/18/16
- * Time: 10:53 AM
- */
+<div class="form-group">
+    {{ Form::label('shelter_name', 'Shelter Name') }}
+    {{ Form::text('shelter_name', '', ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('shelter_address', 'Shelter Address') }}
+    {{ Form::text('shelter_address', '', ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('shelter_phone', 'Shelter Phone') }}
+    {{ Form::text('shelter_phone', '', ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
+</div>
